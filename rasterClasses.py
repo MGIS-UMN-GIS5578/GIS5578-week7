319758d7560b8dd38ea50bee938649103700c393
dahaynes <dahaynes@umn.edu>
2019-10-17 17:01:44 -0500

    updated for 2019

--- rasterClasses.py
+++ rasterClasses.py
@@ -7,14 +7,14 @@ Created on Mon Oct 22 12:52:19 2018
 
 class Raster(object):
     
-    def __init__(self, point, res, rows=10, cols=10):
+    def __init__(self, thePoint, res, rows=10, cols=10):
         """
         A nice doc string here
         """
-        if isinstance(point, Point):
-            self.point = point
-            self.x = self.point.x
-            self.y = self.point.y
+        if isinstance(thePoint, Point):
+            self.thePoint = thePoint
+            self.x = self.thePoint.x
+            self.y = self.thePoint.y
             
         else:
             print("Not a valid point object")
@@ -28,12 +28,12 @@ class Raster(object):
     
 class DataRaster(Raster):
 
-    def __init__(self, point, res, dataset):
+    def __init__(self, thePoint, res, dataset):
         import numpy as np
         self.data = np.array(dataset)
         rows, cols = self.data.shape
         #Old style for inheriting
         #Raster.__init__(self,point, res, rows, cols)
         #New style inheriting using super
-        super().__init__(point, res, rows, cols)
+        super().__init__(thePoint, res, rows, cols)
         
